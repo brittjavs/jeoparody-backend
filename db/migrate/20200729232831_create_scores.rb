8a8909fee6acc3e75a087246045c59942b1a1082
Brittany Javalera <brittjavalera@gmail.com>
2020-07-29 16:53:50 -0700

    added user_id to scores, ran migrations to create schema

--- db/migrate/20200729232831_create_scores.rb
+++ db/migrate/20200729232831_create_scores.rb
@@ -2,7 +2,7 @@ class CreateScores < ActiveRecord::Migration[6.0]
   def change
     create_table :scores do |t|
       t.integer :game_total
-
+      t.integer :user_id
       t.timestamps
     end
   end
