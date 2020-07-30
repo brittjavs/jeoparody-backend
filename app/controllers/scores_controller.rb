35faca7ca4104a05074aaa68fef3d22e5787a717
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-29 20:18:38 -0500

    added CRUD skeleton

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,23 @@
 class ScoresController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+    private
+
+    def score_params
+        params.require(:user).permit(:game_total)
+        # Is it password or password_digest here?
+    end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+        # Is it password or password_digest here?
+    end
+
 end
