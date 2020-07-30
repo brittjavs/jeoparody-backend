d7198cbd4c79d442a9ca02aa0246a873b5091612
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-29 20:15:36 -0500

    created basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :scores
 end
+
+# Do we want to namespace the routes?
+# Do we want it to be nested?
