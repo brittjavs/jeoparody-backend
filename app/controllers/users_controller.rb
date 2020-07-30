0e142e59d80146a39b22874fe9c380b7de08f7ab
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-30 16:25:20 -0500

    added basic CRUD on User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,34 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :destroy]
+
     def index
+        @users = User.all 
+        render json: @users
     end
 
     def show
+        # @user = User.find_by(id:params[:id])
+        render json: @user , status: 200
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            # Do we want to use sessions?
+            # session[:user_id] = @user.id
+            render json: @user, status: 200
     end
 
     def destroy
+        @user.destroy
     end
 
     private
+
+    def set_user
+        @user = User.find_by(id:params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:username, :password)
