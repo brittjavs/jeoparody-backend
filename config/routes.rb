16d0c925a4c5b0bc1f3c036bd0b0a491e2ecb90b
Brittany Javalera <brittjavalera@gmail.com>
2020-08-02 15:31:29 -0700

    added decode_token to index to check if a user is logged in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/signup', to: "users#signup"
       post '/login', to: "sessions#login"
-      resources :scores, only: [:index]
+      resources :scores, only: [:create, :index]
       resources :users, only: [:show, :index, :destroy]
     end
   end
