637380d52866a5b9f2f9fdb3a58e1d9863bbaddc
Khris Punzalan <khrispunzalan@gmail.com>
2020-08-03 14:14:09 -0500

    moved login to users controller to test jwt

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
 # require 'JsonWebToken'
 # require 'JWT'
-=======
-require 'json_web_token'
-
->>>>>>> 16d0c925a4c5b0bc1f3c036bd0b0a491e2ecb90b
 class Api::V1::SessionsController < ApplicationController
-    def login
-        user = User.find_by(username: params[:username])
-        if user && user.authenticate(params[:password])
-<<<<<<< HEAD
-            render json: {token: JsonWebToken.create_token({username: user.username, id:user.id})}
-=======
-            render json: { token: JsonWebToken.create_token(username: user.username, id: user.id)}, status: 200
->>>>>>> 16d0c925a4c5b0bc1f3c036bd0b0a491e2ecb90b
-        else
-            render json: {errors: {message: "Unable to find a user with that username and password"}}, status: 500
-        end
-    end
+    # from video
+    # def login
+    #     user = User.find_by(username: params[:username])
+    #     if user && user.authenticate(params[:password])
+    #         render json: { token: JsonWebToken.create_token(username: user.username, id: user.id)}, status: 200
+    #     else
+    #         render json: {errors: {message: "Unable to find a user with that username and password"}}, status: 500
+    #     end
+    # end
+
+
+
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,46 +1,58 @@
-<<<<<<< HEAD
 # require 'JsonWebToken'
 # require 'JWT'
-=======
-require 'json_web_token'
-
->>>>>>> 16d0c925a4c5b0bc1f3c036bd0b0a491e2ecb90b
 class Api::V1::UsersController < ApplicationController
 
     # before_action :set_user, only: [:show, :destroy]
 
-    def signup
-        params.inspect
-        user = User.new(user_params)
-        if user.save
-            render json: { token: JsonWebToken.create_token(username: user.username, id: user.id)}, status: 200
+    # from article:
+    before_action :authorized, only: [:auto_login]
+   
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
         else
-            render json: {errors: user.errors.full_messages}, status: 500
+          render json: {error: "Invalid username or password"}
         end
-    end
+      end
+
+      def auto_login
+        render json: @user
+      end
+
+                # from the article:
+    def login
+        @user = User.find_by(username: params[:username])
+    
+        if @user && @user.authenticate(params[:password])
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
+        else
+          render json: {error: "Invalid username or password"}
+        end
+      end
+
+
+
+    # def signup
+    #     params.inspect
+    #     user = User.new(user_params)
+    #     if user.save
+    #         render json: { token: JsonWebToken.create_token(username: user.username, id: user.id)}, status: 200
+    #     else
+    #         render json: {errors: user.errors.full_messages}, status: 500
+    #     end
+    # end
 
     def index
         users = User.all 
         render json: users
     end
 
-<<<<<<< HEAD
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            # Do we want to use sessions?
-            # session[:user_id] = @user.id
-            # --we might not need sessions if we use jwt...still learning
-            # binding.pry
-            render json: {token: JsonWebToken.create_token({username: user.username, id:user.id})}
-        else
-            render json: {errors: @user.errors.full_messages}, status: 500
-        end
-=======
     def show
         user = User.find_by(id:params[:id])
         render json: user , status: 200
->>>>>>> 16d0c925a4c5b0bc1f3c036bd0b0a491e2ecb90b
     end
 
     def destroy
