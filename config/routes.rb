810c354d73f83102b5a500145df911b71c282535
Khris Punzalan <khrispunzalan@gmail.com>
2020-08-03 14:13:09 -0500

    changed routes & added seed file to test article

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post '/signup', to: "users#signup"
-      post '/login', to: "sessions#login"
+      # post '/signup', to: "users#signup"
+      # testing out the article:
+      # post '/login', to: "sessions#login"
+
+      # From this article: https://medium.com/better-programming/build-a-rails-api-with-jwt-61fb8a52d833
+      resources :users, only: [:show, :create, :index, :destroy]
+      post "/login", to: "users#login"
+      post '/signup', to: "users#create"
+      get "/auto_login", to: "users#auto_login"
+
+
       resources :scores, only: [:create, :index]
-      resources :users, only: [:show, :index, :destroy]
     end
   end
 end
-
-# Do we want to namespace the routes?
-# Do we want it to be nested?
-  # scores would be separate so we could index highest scores from all users
 
