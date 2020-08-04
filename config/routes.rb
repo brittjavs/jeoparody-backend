e990808d30501f368c26056e9b783c7a9b42328f
Khris Punzalan <khrispunzalan@gmail.com>
2020-08-04 12:40:55 -0500

    changed port & edited routes to use sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # post '/signup', to: "users#signup"
-      # testing out the article:
-      # post '/login', to: "sessions#login"
-
-      # From this article: https://medium.com/better-programming/build-a-rails-api-with-jwt-61fb8a52d833
-      resources :users, only: [:show, :create, :index, :destroy]
-      post "/login", to: "users#login"
-      post '/signup', to: "users#create"
-      get "/auto_login", to: "users#auto_login"
+ 
+      resources :users
+      post "/login", to: "sessions#login"
+      post "/signup", to: "users#create"
+      delete "/logout", to: "sessions#logout"
+      get "get_current_user", to: "sessions#get_current_user"
 
 
       resources :scores, only: [:create, :index]
