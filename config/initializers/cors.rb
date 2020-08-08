8a331e41bbd9cfd0ad9d1ed060bf2eba87f0ce2e
Brittany Javalera <brittjavalera@gmail.com>
2020-08-08 14:51:13 -0700

    added credentials and whitelisted localhost3000 in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http;//localhost:3000'
     # change this to localhost:3000 when react app has been created
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      # credentials: true
+      credentials: true
   end
 end
