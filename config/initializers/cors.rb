21e1a4b6156b7edc63452be7e3a5b412283a44a4
Brittany Javalera <brittjavalera@gmail.com>
2020-08-08 15:04:33 -0700

    fixed a typo in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http;//localhost:3000'
+    origins 'http://localhost:3000'
     # change this to localhost:3000 when react app has been created
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
 end
